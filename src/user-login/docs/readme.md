c5bc05b6d800293243ca5708edc1c2caa96c5cb6
Jan Papousek <jan.papousek@gmail.com>
2015-05-22 13:03:35 +0200

    introduce login-button directive

---
+++ src/user-login/docs/readme.md
@@ -0,0 +1,1 @@
+The login-button allows you to combine 'Sign in' and 'Sign up' actions into one dialog. It supports signing in via Google, Facebook and e-mail.
---
+++ src/user-login/login.js
@@ -0,0 +1,106 @@
+var m = angular.module('proso.apps.user-login', ['ui.bootstrap', 'proso.apps.gettext', 'proso.apps.user-user', 'angulartics', 'angulartics.google.analytics']);
+
+m.controller('LoginController', ['$scope', '$modalInstance', 'signupModal', 'userService', 'gettext', '$analytics',
+    function ($scope, $modalInstance, signupModal, userService, gettext, $analytics) {
+
+    $scope.credentials = {};
+    $scope.alerts = [];
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+    $scope.loginGoogle = function() {
+        $analytics.eventTrack('click', {
+            category: 'login',
+            label: '/login/google',
+        });
+        userService.loginGoogle();
+    };
+
+    $scope.loginFacebook = function() {
+        $analytics.eventTrack('click', {
+            category: 'login',
+            label: '/login/facebook',
+        });
+        userService.loginFacebook();
+    };
+
+    $scope.loginEmail = function() {
+        $analytics.eventTrack('click', {
+            category: 'login',
+            label: '/login/email',
+        });
+        userService
+            .login($scope.credentials.name, $scope.credentials.password)
+            .error($scope.onError);
+    };
+
+    $scope.openSignupModal = function() {
+        $scope.cancel();
+        signupModal.open();
+    };
+
+    $scope.signup = function() {
+        $analytics.eventTrack('click', {
+            category: 'signup',
+            label: '/signup/email',
+        });
+        userService.signupParams(
+            $scope.credentials.username,
+            $scope.credentials.email,
+            $scope.credentials.password,
+            $scope.credentials.password_check,
+            $scope.credentials.first_name,
+            $scope.credentials.last_name
+        );
+    };
+
+    $scope.onError = function(error) {
+        $analytics.eventTrack('error', {
+            category: 'login',
+            label: 'error/login'
+        });
+        $scope.alerts.push({
+            type: error.type || 'danger',
+            msg: error.msg || gettext('Something wrong has happened.')
+        });
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+}]);
+
+m.factory('signupModal', ['$modal', function($modal) {
+    return {
+        open: function() {
+            $modal.open({
+                templateUrl: 'templates/user-login/signup-modal.html',
+                controller: 'LoginController',
+            });
+        }
+    };
+}]);
+
+m.factory('loginModal', ["$modal", function($modal) {
+    return {
+        open: function() {
+            $modal.open({
+                templateUrl: 'templates/user-login/login-modal.html',
+                controller: 'LoginController',
+            });
+        }
+    };
+}]);
+
+m.directive('loginButton', ['loginModal', function(loginModal) {
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            element.bind('click', function(){
+                loginModal.open();
+            });
+        }
+    };
+}]);
