ba9d5ad195636ae38b811ddf6becc6bfe186f44f
Jan Papousek <jan.papousek@gmail.com>
2015-08-04 16:39:43 +0200

    add audit chart to proso toolbar

--- src/common-toolbar/toolbar.js
+++ src/common-toolbar/toolbar.js
@@ -1,6 +1,6 @@
 var m = angular.module('proso.apps.common-toolbar', ['ngCookies', 'proso.apps.common-config']);
 
-m.controller("ToolbarController", ['$scope', '$cookies', 'configService', 'loggingService', '$timeout', function($scope, $cookies, configService, loggingService, $timeout) {
+m.controller("ToolbarController", ['$scope', '$cookies', 'configService', 'loggingService', '$timeout', '$http', function($scope, $cookies, configService, loggingService, $timeout, $http) {
     $scope.override = configService.override;
     $scope.removeOverridden = configService.removeOverridden;
     $scope.date = new Date();
@@ -36,6 +36,58 @@ m.controller("ToolbarController", ['$scope', '$cookies', 'configService', 'loggi
             delete overridden[k];
         });
         return overridden;
+    };
+
+    $scope.showAuditChart = function() {
+        var params = {};
+        if ($scope.auditLimit) {
+            params['limit'] = $scope.auditLimit;
+        }
+        if ($scope.auditUser) {
+            params['user'] = $scope.auditUser;
+        }
+        $http.get("/models/audit/" + $scope.auditKey, {params: params})
+            .success(function(response) {
+                var data = new google.visualization.DataTable();
+                data.addColumn('number', 'Update');
+                data.addColumn('number', 'Value');
+                data.addColumn({type: 'datetime', role: 'tooltip'});
+                response.data.reverse();
+                var rows = [];
+                for (var i = 0; i < response.data.length; i++) {
+                    var record = response.data[i];
+                    rows.push([i, record.value, new Date(record.time)]);
+                }
+                data.addRows(rows);
+                var options = {
+                    title: $scope.auditKey,
+                    legend: {
+                        position: 'none'
+                    },
+                    vAxis: {
+                        format: '#.###'
+                    },
+                    hAxis: {
+                        title: 'Time',
+                        position: 'center'
+                    },
+                    pointSize: 10,
+                    series: {
+                        0: {
+                            pointShape: 'diamond'
+                        }
+                    },
+                    width: 450,
+                    height: 300,
+                    'chartArea': {'width': '90%', 'height': '90%'}
+                };
+                var formatter = new google.visualization.NumberFormat({
+                    fractionDigits: 3, pattern: '#.###'
+                });
+                formatter.format(data, 1);
+                var chart = new google.visualization.LineChart(document.getElementById('auditChart'));
+                chart.draw(data, options);
+            });
     };
 
 }]);
