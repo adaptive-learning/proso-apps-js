e7959b275d06d04e9a1c9f9479b41462465125eb
Thran <exthran@gmail.com>
2015-10-05 16:52:06 +0200

    fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function(grunt) {
                     'git push origin',
 
                     'git checkout master-<%= version.major %>.<%= version.minor %>.X',
-                    'sed -i "s/var master = true;/var version = false;/g" Gruntfile.js',
+                    'sed -i "s/var master = t..e;/var master = false;/g" Gruntfile.js',
                     'git add Gruntfile.js',
                     'git commit -m "new branch for version <%= version.major %>.<%= version.minor %>.X"',
                     'git push origin master-<%= version.major %>.<%= version.minor %>.X'
