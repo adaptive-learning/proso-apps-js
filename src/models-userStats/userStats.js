7b0fd16b5b373237a0c8e5e311c45913c2cd65ba
Vit Stanislav <slaweet@seznam.cz>
2016-05-20 13:00:48 +0200

    Migrate flashcards-userStats to models-userStats

--- src/models-userStats/userStats.js
+++ src/models-userStats/userStats.js
@@ -1,42 +1,40 @@
-var m = angular.module('proso.apps.flashcards-userStats', ['ngCookies']);
+var m = angular.module('proso.apps.models-userStats', ['ngCookies']);
 m.service("userStatsService", ["$http", "$cookies", function($http, $cookies){
     var self = this;
 
-    var filters = {};
+    var filters = {
+      filters: {},
+    };
 
     self.addGroup = function (id, data) {
         if (!data.language){
             delete data.language;
         }
-        filters[id] = data;
+        filters.filters[id] = data;
     };
 
-    self.addGroupParams = function (id, categories, contexts, types, language) {
-        filters[id] = {
-            categories: categories,
-            contexts: contexts,
-            types: types
-        };
+    self.addGroupParams = function (id, filter, language) {
+        filters.filters[id] = filter;
         if (typeof language !== "undefined"){
-            filters[id].language = language;
+            filters.language = language;
         }
     };
 
-    self.getFlashcardCounts = function(){
+    self.getToPracticeCounts = function(){
         $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
-        return $http.post("/flashcards/flashcard_counts/", filters, {cache: true});
+        return $http.post("/models/to_practice_counts/", filters, {cache: true});
     };
 
     self.getStats = function(mastered, username){
         $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
-        var params = {filters: JSON.stringify(filters)};
+        var params = {filters: JSON.stringify(filters.filters)};
         if (mastered){
             params.mastered = true;
         }
         if (username){
             params.username = username;
         }
-        return $http.get("/flashcards/user_stats/", {params: params});
+        return $http.get("/models/user_stats/", {params: params});
     };
 
     self.getStatsPost = function(mastered, username){
@@ -44,7 +42,7 @@ m.service("userStatsService", ["$http", "$cookies", function($http, $cookies){
         var params = "?";
         params += mastered ? "&mastered=true" : "";
         params += username ? "&username="+username : "";
-        return $http.post("/flashcards/user_stats/" + params, filters);
+        return $http.post("/models/user_stats/" + params, filters);
     };
 
     self.clean = function(){
