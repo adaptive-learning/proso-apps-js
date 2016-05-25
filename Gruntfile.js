a8da743adefdbfe7326bd9d93af8b0c1673bef68
Jan Papousek <jan.papousek@gmail.com>
2016-05-25 09:27:13 +0200

    start working on 2.1.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 module.exports = function(grunt) {
     'use strict';
-    var version = '2.1.0';
+    var version = '2.1.1';
     var master = false;
     var version_parts = version.split(".");
     version_parts = {
